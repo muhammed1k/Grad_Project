aeddcde1c1b32b4c82cb61a1cabd821b2f9e827f
unknown <mohamedkelbrolosy@gmail.com>
2022-07-22 03:31:00 +0200

    v3.4

--- models/notify.js
+++ models/notify.js
@@ -7,6 +7,7 @@ var userSchema = mongoose.Schema({
     notifaction:{type:String},
     date:{type:Date,default:Date.now},
     driverid: { type: mongoose.Schema.Types.ObjectId, ref: 'users'},
+    riderid: { type: mongoose.Schema.Types.ObjectId, ref: 'users'}
     
     
 });
--- routes/notify.js
+++ routes/notify.js
@@ -5,9 +5,11 @@ var router = express.Router();
 
 router.post("/notify_driver", async(req,res) => {
     const driver = req.body.driverid
+    const rider = req.body.riderid
     const msg = req.body.msg
     var newNotifaction = new Notify({
         driverid:driver,
+        riderid:rider,
         notifaction:msg
     })
     
@@ -24,7 +26,9 @@ router.post("/notify_driver", async(req,res) => {
   })
   
 router.get("/get_notify", async(req,res) => {
-    const notify = await Notify.find({driverid:req.query.driverid}).populate({path:'driverid',model:'User'})
+    const notify = await Notify.find({driverid:req.body.driverid})
+    .populate({path:'driverid',model:'User'})
+    .populate({path:'riderid',model:'User'})
     res.status(200).json(notify);
 })
 
